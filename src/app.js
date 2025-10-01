97612aa60e864f647571e6ca88cc3a0534da1677
andrecodetech <iki.andre@escola.pr.gov.br>
2025-10-01 14:19:27 +0000

    segundo commti teste

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-ddddd
+const express = require("express");
+const app = express();
+
+// Middleware para interpretar JSON
+app.use(express.json());
+
+// Primeira rota
+app.get("/", (req, res) => {
+  res.send("API com Express funcionando!");
+});
+
+// Porta de execução
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
